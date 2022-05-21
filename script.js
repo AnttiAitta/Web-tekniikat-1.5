2badb0c0908ed68227919509948a8da6a292037c
AnttiAitta <n1aian00@students.oamk.fi>
2022-05-21 16:07:25 +0300

    Final!

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //Events
 
 let image = document.getElementById("homeimg");
-
-image.addEventListener("mouseenter", reverseColor);
-image.addEventListener("mouseleave", reverseColor);
-image.addEventListener("click", changeImage);
+if(image != null) {
+    image.addEventListener("mouseenter", reverseColor);
+    image.addEventListener("mouseleave", reverseColor);
+    image.addEventListener("click", changeImage);
+}
 
 function reverseColor() {
     image.classList.toggle("imagecolor");
@@ -19,3 +20,12 @@ let h1 = document.querySelector("h1");
 h1.classList.add("special");
 h1.style.width = "80%";
 
+//JSON-REST
+fetch("https://yesno.wtf/api")
+  .then(response => response.json())
+  .then(
+      data => {
+          document.getElementById("print-here").innerHTML =
+      "<h2>" + data.answer + "</h2>";
+      }
+  );
